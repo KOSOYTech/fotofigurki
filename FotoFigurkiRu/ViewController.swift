ce48218db0b39c028d1aa4abc51467a824dda314
Maxim Kosoy <info@korolev-site.ru>
2020-06-24 02:52:17 +0300

    With CocoaPods

---
+++ FotoFigurkiRu/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  FotoFigurkiRu
+//
+//  Created by Maxim Kosoy on 08.03.2020.
+//  Copyright © 2020 Maxim Kosoy. All rights reserved.
+//
+
+import UIKit
+import ImagePicker
+
+class ViewController: UIViewController, ImagePickerDelegate {
+    
+    func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        <#code#>
+    }
+    
+    func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        <#code#>
+    }
+    
+    func cancelButtonDidPress(_ imagePicker: ImagePickerController) {
+        <#code#>
+    }
+    
+
+    // Инициализация полей ввода и кнопки отправки заказа
+    @IBOutlet weak var kakObr: UITextField!
+    @IBOutlet weak var kakSvyaz: UITextField!
+    @IBAction func makeOrder(_ sender: UIButton) {
+        
+        // Отправка данных на сервер
+        let url = URL(string: "https://fotofigurki.ru/appsend.php")!
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        components.queryItems = [
+            URLQueryItem(name: "name", value: kakObr.text!),
+            URLQueryItem(name: "svyaz", value: kakSvyaz.text!)
+        ]
+        let query = components.url!.query
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = Data(query!.utf8)
+        
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                // Check for Error
+                if let error = error {
+                    print("Error took place \(error)")
+                    return
+                }
+         
+                // Convert HTTP Response Data to a String
+                if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    print("Response data string:\n \(dataString)")
+                }
+        }
+        task.resume()
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+ 
+    }
+
+
+}
---
+++ Pods/Target Support Files/Pods-FotoFigurkiRu/Pods-FotoFigurkiRu-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FotoFigurkiRu : NSObject
+@end
+@implementation PodsDummy_Pods_FotoFigurkiRu
+@end
