8ff6254e8dd2a093f3159252ef85b8adfba5d8cc
Maxim Kosoy <info@korolev-site.ru>
2020-06-24 04:04:49 +0300

    CocoaComments 7

--- FotoFigurkiRu/ViewController.swift
+++ FotoFigurkiRu/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, ImagePickerDelegate {
         <#code#>
     }
     
-    // Указываем, что необходимо сделать, когда пользователь выбрал фото
+    // Указываем, что необходимо сделать, когда пользователь выбрал фото и нажал
     func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
         <#code#>
     }
